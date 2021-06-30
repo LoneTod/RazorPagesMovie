0362f4e4b79378918af08355db62e9456dbd97be
LoneTod <tarcizio_melo@hotmail.com>
2021-06-30 02:00:50 -0300

    Initial commit

---
+++ RazorPagesMovie/Data/RazorPagesMoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovies.Modelos;
+
+namespace RazorPagesMovies.Data
+{
+    public class RazorPagesMoviesContext : DbContext
+    {
+        public RazorPagesMoviesContext (DbContextOptions<RazorPagesMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovies.Modelos.Movie> Movie { get; set; }
+    }
+}
